f21fd0220756610a8022984e6733229fe8508ee7
agbossi <abossi@itba.edu.ar>
2022-10-03 05:55:10 -0300

    actualizacion gitignore

--- dump.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.tp_nlp_app_token(id, value, auth_to) VALUES (1, 'AIzaSyCqFKP8FWAeRPV8uvS8fs9I5yVCiY9MzDc', 'maps.googleapis.com');
-INSERT INTO public.tp_nlp_app_token(id, value, auth_to) VALUES (2, 'Z29vZ2xlLW9hdXRoMnwxMDIwNDI1NjYxMDE1MDI0ODAxNTB8ZjBkZWI0NDYzYw', 'api.app.outscraper.com');
-
-INSERT INTO public.tp_nlp_app_querytype(id, "sortBy") VALUES (1, 'most_relevant');
-INSERT INTO public.tp_nlp_app_querytype(id, "sortBy") VALUES (2, 'newest');
-INSERT INTO public.tp_nlp_app_querytype(id, "sortBy") VALUES (3, 'highest_rating');
-INSERT INTO public.tp_nlp_app_querytype(id, "sortBy") VALUES (4, 'lowest_rating');
